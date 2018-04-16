36573082d14fb370491931a94592873b53c8ceaa
18715 <18715@LAPTOP-HSNN5GTO>
2018-04-16 19:35:47 +0800

    springboot-demo

---
+++ src/main/java/com/song/springboot/sptingbootdemo/SptingbootDemoApplication.java
@@ -0,0 +1,12 @@
+package com.song.springboot.sptingbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptingbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SptingbootDemoApplication.class, args);
+	}
+}
